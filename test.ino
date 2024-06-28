856c0d746b097116f5db3ee6980760d9447d5bfc
AnndySK <andrejklein123@gmail.com>
2024-06-28 22:15:07 +0200

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,20 +1,17 @@
-#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // Změňte adresu dle výsledku skenování.
 
 void setup() {
-  //pinMode(SDA, INPUT_PULLUP);
-  //pinMode(SCL, INPUT_PULLUP);
-  lcd.begin(16, 2);
+  pinMode(SDA, INPUT_PULLUP);
+  pinMode(SCL, INPUT_PULLUP);
+  lcd.init();
+  lcd.backlight();
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("Hello, World!");
 }
 
 void loop() {
-  lcd.backlight();
-  delay(3000);
-  lcd.noBacklight();
-  delay(3000);
+  // code
 }
